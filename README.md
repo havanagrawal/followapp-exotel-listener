f7a0ef5c7a26d6c1ececb770df499b0a8bf599e2
Havan <havanagrawal@gmail.com>
2016-07-17 19:12:28 +0530

    Add new service according to Exotel API, reformat code

--- README.md
+++ README.md
@@ -13,5 +13,22 @@ There are two service endpoints that this service exposes:
 
 A .war file will be generated, which can be deployed to the container of your choice. (I use Tomcat 9)
 
+## Using POSTMAN
+To test these services, I use the Chrome plugin POSTMAN
+Some example URL's
+    GET: http://localhost:8080/exotelListener/webapi/exotel/audioresponse
+    HEAD: http://localhost:8080/exotelListener/webapi/exotel/audioresponse
+    GET: http://localhost:8080/exotelListener/webapi/exotel/audiomessage
+    GET: http://localhost:8080/exotelListener/webapi/exotel/userinput?digits=2
 
+## Documentation for Exotel API
+
+1. http://support.exotel.in/support/solutions/articles/48283-working-with-passthru-applet
+2. http://support.exotel.in/support/solutions/articles/48285-greeting-using-dynamic-text-or-audio-from-url
+
+## TODO List
+	1. Look for a file sharing/hosting service for audio files
+	2. Implement logic for retrieving audio files based on input params (currently, all QueryParams are ignored)
+	3. Write tests
+	4. Deploy this war on a publicly hosted container (Heroku/Openshift or some other)
 
--- src/main/java/com/followapp/UserInputListenerService.java
+++ src/main/java/com/followapp/UserInputListenerService.java
@@ -3,7 +3,6 @@ package com.followapp;
 import java.io.File;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -17,38 +16,69 @@ import javax.ws.rs.core.Response;
 public class UserInputListenerService {
 
 	public static final MediaType WAV = new MediaType("audio", "wav");
-	
-    /**
-     * Handle user input from the "Gather" flow of the Exotel API
-     *
-     * @return A 200-OK Response if the input was processed sucessfully
-     */
-	
-    @GET
-    @Path("userinput")
-    public Response getResponseOfUser(@QueryParam("digits") String input) {
-    	// TODO: Persist the response to the DB
-    	// TODO: Send a 302 Response in case the user presses 2
-    	System.out.println("We got query parameter: " + input);
-    	return Response.status(Response.Status.OK).build();
-    }
-    
-    @GET
-    @Path("audiomessage")
-    @Produces("audio/wav")
-    public Response getAudio(@QueryParam("From") String user) {
-    	// TODO: Figure out how we store these audio files
-    	// For now, we return a static file
-    	try {
-    		File message = new File(this.getClass().getResource("/audio/havan.wav").getFile().replace("%20", " "));
-    		return Response.ok(message, WAV)
-    				.build();
-    	}
-    	catch (NullPointerException npe) {
-    		npe.printStackTrace();
-    		return Response.status(Response.Status.NO_CONTENT)
-    				.build();
-    	}
-    	//return Response.status(Response.Status.OK).build();
-    }
+
+	/**
+	 * Handle user input from the "Gather" flow of the Exotel API
+	 *
+	 * @return A 200-OK Response if the input was processed successfully
+	 */
+
+	@GET
+	@Path("userinput")
+	public Response getResponseOfUser(@QueryParam("digits") String input) {
+		// TODO: Persist the response to the DB
+		// TODO: Send a 302 Response in case the user presses 2
+		System.out.println("We got query parameter: " + input);
+		return Response.status(Response.Status.OK).build();
+	}
+
+	/**
+	 * Return a Response containing a link to the audio file
+	 * 
+	 * http://support.exotel.in/support/solutions/articles/48285-greeting-using-
+	 * dynamic-text-or-audio-from-url
+	 * 
+	 * Note: The HEAD request is handled automatically
+	 * 
+	 * @param user
+	 * @return
+	 */
+	@GET
+	@Path("audioresponse")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getAudioResponse(@QueryParam("CallSid") String callSid, 
+									 @QueryParam("From") String from,
+									 @QueryParam("To") String exotelNumber, 
+									 @QueryParam("DialWhomNumber") String beingCalled) {
+		return Response.ok("http://localhost:8080/exotelListener/webapi/exotel/audiomessage", MediaType.TEXT_PLAIN_TYPE)
+				.build();
+	}
+
+	/**
+	 * Send a Response containing the audio file in WAV format
+	 * 
+	 * This may not be required if we find a audio hosting/file sharing service
+	 * Which does not require login/authentication to retrieve files
+	 * 
+	 * @param user
+	 * @return
+	 */
+	@GET
+	@Path("audiomessage")
+	@Produces("audio/wav")
+	public Response getAudio(@QueryParam("CallSid") String callSid, 
+							 @QueryParam("From") String from,
+							 @QueryParam("To") String exotelNumber, 
+							 @QueryParam("DialWhomNumber") String beingCalled) {
+		// TODO: Figure out how we store these audio files
+		// TODO: Write logic to retrieve file based on params
+		// For now, we return a static file
+		try {
+			File message = new File(this.getClass().getResource("/audio/havan.wav").getFile().replace("%20", " "));
+			return Response.ok(message, WAV).build();
+		} catch (NullPointerException npe) {
+			npe.printStackTrace();
+			return Response.status(Response.Status.NO_CONTENT).build();
+		}
+	}
 }
